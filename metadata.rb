53fec3a82eaaf94cf31ad37ad45ff11cc7d4b01d
Alex Crawford <acrawford@cmdagency.com>
2014-02-20 16:26:23 -0800

    cookbooks to depends in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,10 @@ description      'Installs/Configures precise64-lamp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apt'
+depends 'build-essential'
+depends 'git'
+depends 'php'
+depends 'apache2'
+depends 'mysql'
+depends 'database'
